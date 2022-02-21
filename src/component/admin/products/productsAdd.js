3a0cf3003fd784b99b868847070f99895a4b18e2
dacphuongotp <dacphuongotp@gmail.com>
2022-02-21 15:11:38 +0700

    update validate form addproducts

--- src/component/admin/products/productsAdd.js
+++ src/component/admin/products/productsAdd.js
@@ -1,13 +1,18 @@
 import NavAdmin from "../../navadmin";
 import {
-    add
+  add
 } from "../../../api/product";
 import axios from "axios";
+import $ from "jquery";
+import validate from "jquery-validation";
+import {
+  rule
+} from "postcss";
 
 
 const ProductsAdd = {
-    render() {
-        return /* html */ `
+  render() {
+    return /* html */ `
          ${NavAdmin.render()}
          <header class="bg-white shadow">
             <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
@@ -30,7 +35,7 @@ const ProductsAdd = {
                               Title
                             </label>
                             <div class="mt-1 flex rounded-md shadow-sm">
-                              <input type="text" name="title-post" id="title-products" class="py-2 px-2 focus:ring-indigo-500 focus:border-indigo-500 flex-1 block w-full rounded-none rounded-r-md sm:text-sm border-gray-300" placeholder="Title">
+                              <input type="text" name="title-products" id="title-products" class="py-2 px-2 focus:ring-indigo-500 focus:border-indigo-500 flex-1 block w-full rounded-none rounded-r-md sm:text-sm border-gray-300" placeholder="Title">
                             </div>
                           </div>
                         </div>
@@ -49,7 +54,7 @@ const ProductsAdd = {
                             Desc
                           </label>
                           <div class="mt-1">
-                            <textarea id="desc-products" name="desc-products" rows="3" class="py-2 px-2 shadow-sm focus:ring-indigo-500 focus:border-indigo-500 mt-1 block w-full sm:text-sm border border-gray-300 rounded-md" placeholder="Desc"></textarea>
+                            <textarea id="desc-products" name="desc-products" rows="3" class="py-2 px-2 shadow-sm focus:ring-indigo-500 focus:border-indigo-500 mt-1 block w-5/6 sm:text-sm border border-gray-300 rounded-md" placeholder="Desc"></textarea>
                           </div>
                         </div>
             
@@ -58,7 +63,7 @@ const ProductsAdd = {
                             <label for="img-products" class="block text-sm font-medium text-gray-700">
                             Ảnh
                           </label>
-                          <input type="file" id="img-products">
+                          <input name="img-products" type="file" id="img-products">
                           </div>
                         </div>
                       </div>
@@ -78,40 +83,66 @@ const ProductsAdd = {
         </div>
         <div>
         `;
-    },
-    afterRender() {
-        const formAddProducts = document.querySelector('#form-add-products');
-        const imgProducts = document.querySelector("#img-products");
+  },
+  afterRender() {
+    const formAddProducts = $('#form-add-products');
+    const imgProducts = document.querySelector("#img-products");
 
-        const CLOUDINARY_PRESET = "cloud1";
-        const CLOUDINARY_API_URL = "https://api.cloudinary.com/v1_1/dqhtmst8q/image/upload";
+    const CLOUDINARY_PRESET = "cloud1";
+    const CLOUDINARY_API_URL = "https://api.cloudinary.com/v1_1/dqhtmst8q/image/upload";
 
-        formAddProducts.addEventListener("submit", async function (e) {
-            e.preventDefault();
-            const file = imgProducts.files[0];
 
-            const formData = new FormData();
+    formAddProducts.validate({
+      rules: {
+        "title-products": {
+          required: true,
+          minlength: 5
+        },
+        "price-products": {
+          required: true,
+          minlength: 1,
+          number: true,
+        }
+      },
+      messages: {
+        "title-products": {
+          required: " ---  Vui lòng điền thông tin!",
+          minlength: "Ít nhất phải trên 5 ký tự"
+        },
+        "price-products": {
+          required: " ---  Vui lòng điền thông tin!",
+          minlength: "Ít nhất phải trên 2 ký tự"
+        }
+      },
+      submitHandler: () => {
+        async function handleAddPost() {
+          const file = imgProducts.files[0];
 
-            formData.append("file", file);
-            formData.append("upload_preset", CLOUDINARY_PRESET);
+          const formData = new FormData();
 
-            // call api cloudinary
-            const {
-                data
-            } = await axios.post(CLOUDINARY_API_URL, formData, {
-                headers: {
-                    "Content-Type": "application/form-data"
-                }
-            })
-            // call api thêm bài viết
-            add({
-                title: document.querySelector("#title-products").value,
-                price: document.querySelector("#price-products").value,
-                img: data.url,
-                desc: document.querySelector("#desc-products").value,
-            })
-            document.location.href = "/admin/products";
-        });
-    }
+          formData.append("file", file);
+          formData.append("upload_preset", CLOUDINARY_PRESET);
+
+          // call api cloudinary
+          const {
+            data
+          } = await axios.post(CLOUDINARY_API_URL, formData, {
+            headers: {
+              "Content-Type": "application/form-data"
+            }
+          })
+          // call api thêm bài viết
+          add({
+            title: document.querySelector("#title-products").value,
+            price: document.querySelector("#price-products").value,
+            img: data.url,
+            desc: document.querySelector("#desc-products").value,
+          })
+          document.location.href = "/admin/products";
+        }
+        handleAddPost();
+      }
+    })
+  }
 };
 export default ProductsAdd;
